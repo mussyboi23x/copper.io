1db4ac43096a6d316d5ea4dae13872563beb64ce
thejavakid <thejavakid1@gmail.com>
2016-08-08 16:10:22 +0000

    fixed the undefined error with the model mesh due to a overlooked init var, began to add collidion mechanics with objects

--- client/Player.js
+++ client/Player.js
@@ -4,10 +4,22 @@ function player(){
     this.posX=0;
     this.posY=0;
     this.posZ=0;
+    this.inventory = [];
     this.kill = function(){
         player.heath=0;
     }
     this.setPos = function(x,y,z){
-        
+        this.posX = y;
+        this.posY = y;
+        this.posZ = z;
+    }
+    this.getItem = function(index){
+        return inventory[index];
+    }
+    this.addItem = function(Item){
+        this.inventory.push(Item);
+    }
+    this.PosArray = function(){
+        return [this.posX, this.posY, this.posY]
     }
 }
--- client/item.js
+++ client/item.js
@@ -1,26 +1,133 @@
+/*
+global MainItemRegistry
+*/
+MainItemRegistry = new ItemRegistry();
+
 function Item(id, name, model){
+    this.id = id;
+    this.model = model;
+    this.name = name;
     this.loadModel = function(){
         model.load();
-    }
+    };
+    this.getModel = function(){
+        return this.model;
+    };
     this.getName = function(){
         return name;
-    }
+    };
     this.setAction = function(code){
         this.action = code();
-    }
+    };
     this.pickUp = function(Player){
         //?
-    }
+    };
     this.putDown = function(Player){
         //?
-    }
+    };
     this.doAction = function(){
         if(this.action() === undefined){
             this.action();
         }
-    }
+    };
     this.removeModel = function() {
     	scene.remove( this.model );
-    	animate();
+    	//animate();
+	};
+	this.editModel = function(edits){
+	    edits(this.getModel());
+	    //animate();
+	};
+	this.detectCollision = function(){
+	    
+	    var vertices = model.getMesh().geometry.vertices;
+	    
+	    
+	    for (var vertexIndex = 0; vertexIndex < player.geometry.vertices.length; vertexIndex++)
+        {       
+            var localVertex = player.geometry.vertices[vertexIndex].clone();
+            var globalVertex = player.matrix.multiplyVector3(localVertex);
+            var directionVector = globalVertex.subSelf( player.position );
+        
+            var ray = new THREE.Ray( player.position, directionVector.clone().normalize() );
+            var collisionResults = ray.intersectObjects( collidableMeshList );
+            if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
+            {
+                // a collision occurred... do something...
+            }
+        }
+	};
+	
+	this.largestX = function(vertices, matrix){
+	    var x;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(x == undefined || vertex.x > x){
+	            x = vertex.x;
+	        }
+	    }
+	    return ;
 	}
+	this.smallestX = function(vertices, matrix){
+	    var x;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(x == undefined || vertex.x < x){
+	            x = vertex.x;
+	        }
+	    }
+	    return x;
+	}
+	this.largestY = function(vertices, matrix){
+	    var y;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(y == undefined || vertex.y > y){
+	            y = vertex.y;
+	        }
+	    }
+	    return y;
+	}
+	this.smallestY = function(vertices, matrix){
+	    var y;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(y == undefined || vertex.y < y){
+	            y = vertex.y;
+	        }
+	    }
+	    return y;
+	}
+	this.largestZ = function(vertices, matrix){
+	    var z;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(z == undefined || vertex.z > z){
+	            z = vertex.z;
+	        }
+	    }
+	    return z;
+	}
+	this.smallestZ = function(vertices, matrix){
+	    var z;
+	    for(var i = 0; i < vertices.length; i++){
+	        var vertex = matrix.multiplyVector3(vertices[i].clone());
+	        if(z == undefined || vertex.z < z){
+	            z = vertex.z;
+	        }
+	    }
+	    return z;
+	}
+}
+
+function ItemRegistry(){
+    this.items = [];
+    /**
+     * this functions distroys all items in
+     * its registry, possably devistating to 
+     * gameplay.
+     */
+     this.destroyItems = function(){
+         for(var i =0; i<this.items.length; i++)this.items[i] = undefined;
+     };
 }
