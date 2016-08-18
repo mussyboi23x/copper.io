85b2a7b2487a54ffd8a721e094c68bf243514ef4
thejavakid <thejavakid1@gmail.com>
2016-08-18 03:24:47 +0000

    about to make major changes to the item file, player file, and index.html so saving previous working version. nothing major has happened.

--- README.md
+++ README.md
@@ -4,37 +4,4 @@
     -----------------------------------------
     
 Copper is a game engine that uses content packs to add all of the games content. these content packs are seperate from the 
-source code and are imported via URL. here is a basic drawing of the order of content packs, game source, textures, and content pack code:
-
-              +-------------+
-              | game        |
-              +------+------+
-                     |
-                     |
-                     |
-                     |
-          +----------+-----------+
-          |                      |
-          |                      |
-          |                      |
-    +-----+-----+          +-----+-----+
-    |default src|          |content    |
-    +-----+-----+          +----+------+
-          |                     |
-          |                     |
-          |                     |
-          |                     |
-          |                     |
-          |                     |
-    +-----+----+                |
-    |  register|                |
-    |  content |        +-------+-----------+      +-------------------+
-    |  and add |        |     content added |      | render and finish |
-    |  hooks   |        |                   |      | loading game. show|
-    |  for other -------+y                  |      | start screen and  |
-    |  content |        |                   |      | allow for player  |
-    +----+-----+        |                   |      | to join a online  |
-         |              +--------n----------+      | room.             |
-         |                       +                 |                   |
-         |                       |                 |                   |
-         +-----------------------+-----------------+-------------------+
+source code and are imported via URL, however can be stand alone if no content packs are added. 
--- client/item.js
+++ client/item.js
@@ -20,12 +20,6 @@ function Item(id, name, model){
     this.setAction = function(code){
         this.action = code();
     };
-    this.pickUp = function(Player){
-        //?
-    };
-    this.putDown = function(Player){
-        //?
-    };
     this.doAction = function(){
         if(this.action() === undefined){
             this.action();
@@ -35,92 +29,10 @@ function Item(id, name, model){
     	scene.remove( this.model );
     	//animate();
 	};
-	this.editModel = function(edits){
+	this.meshFunc = function(edits){
 	    edits(this.getModel());
-	    //animate();
-	};
-	this.detectCollision = function(){
-	    
-	    var vertices = model.getMesh().geometry.vertices;
-	    
-	    
-	    for (var vertexIndex = 0; vertexIndex < player.geometry.vertices.length; vertexIndex++)
-        {       
-            var localVertex = player.geometry.vertices[vertexIndex].clone();
-            var globalVertex = player.matrix.multiplyVector3(localVertex);
-            var directionVector = globalVertex.subSelf( player.position );
-        
-            var ray = new THREE.Ray( player.position, directionVector.clone().normalize() );
-            var collisionResults = ray.intersectObjects( collidableMeshList );
-            if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() ) 
-            {
-                // a collision occurred... do something...
-            }
-        }
 	};
 }
-	
-	function largestX(vertices, matrix){
-	    var x;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(x == undefined || vertex.x > x){
-	            x = vertex.x;
-	        }
-	    }
-	    return x;
-	};
-	
-	function smallestX(vertices, matrix){
-	    var x;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(x == undefined || vertex.x < x){
-	            x = vertex.x;
-	        }
-	    }
-	    return x;
-	};
-	function largestY(vertices, matrix){
-	    var y;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(y == undefined || vertex.y > y){
-	            y = vertex.y;
-	        }
-	    }
-	    return y;
-	};
-	function smallestY(vertices, matrix){
-	    var y;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(y == undefined || vertex.y < y){
-	            y = vertex.y;
-	        }
-	    }
-	    return y;
-	};
-	function largestZ(vertices, matrix){
-	    var z;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(z == undefined || vertex.z > z){
-	            z = vertex.z;
-	        }
-	    }
-	    return z;
-	};
-	function smallestZ(vertices, matrix){
-	    var z;
-	    for(var i = 0; i < vertices.length; i++){
-	        var vertex = matrix.multiplyVector3(vertices[i].clone());
-	        if(z == undefined || vertex.z < z){
-	            z = vertex.z;
-	        }
-	    }
-	    return z;
-	};
 
 function ItemRegistry(){
     this.items = [];
